3dd7129237d83ffb1d1734f30b5a1d956f00d5f0
Cade Gordon <cadegordon093@gmail.com>
2022-01-19 15:17:56 -0600

    commented code

--- NumberGuessingGame/Main.cpp
+++ NumberGuessingGame/Main.cpp
@@ -19,7 +19,7 @@ int main()
 
 	bool stinkyCheater = false;
 
-
+	//tell user to think of a number in range
 	std::cout << "please think of a number 1-100" << std::endl;
 
 
@@ -29,7 +29,7 @@ int main()
 
 	while (isCorrect == false || stinkyCheater)
 	{
-
+		//identifies if the player is cheating
 		if (max == min)
 		{
 			stinkyCheater = true;
@@ -39,31 +39,41 @@ int main()
 			break;
 		}
 
-
+		//selects a random number within the range
 		randomGuess = rand() % (max - min) + min;
 
+		//ask if it is the player number
 		std::cout << "is your number " << randomGuess << " ?" << std::endl;
+		//gets players input
 		std::cout << "if this was your number press 1." << "if not press 2. to keep guessing" << std::endl;
 
 		std::cin >> response;
 
+		//if input is 1 then...
 		if (response == 1)
 		{
+			//...closet the game
 			isCorrect = true;
 		}
+		//else if...
 		else if (response == 2)
 		{
+			//ask if the players number is higher or lower
 			std::cout << "was your number higher or lower?" << std::endl;
 			std::cout << "if the number is higher than the one you are thinking press 1." << "or if the number you are thinking is lower press 2." << std::endl;
 			
 			std::cin >> response;
 
+			//if input is 1...
 			if (response == 1)
 			{
+				//...add to the to the mininmum number in the range
 				min = randomGuess + 1;
 			}
+			// else if...
 			else if (response == 2)
 			{
+				//...
 				max = randomGuess;
 			}
 
@@ -71,9 +81,9 @@ int main()
 		
 		system("cls");
 	}
+	
 
-
-
+	//plays message if the playter cheats
 	if (stinkyCheater)
 	{
 		std::cout << "you are a naughty naughty stinky cheater" << std::endl;
